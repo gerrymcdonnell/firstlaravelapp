c097973a8ae7607f2f8fcb0d7ecc62aee0918eb5
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-06 11:26:12 +0000

    basic route commands

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/ged', function () {  
+    
+    //testing routes
+    return 'hello ged';
+});
+
+
+//default route setting
+Route::get('/', function () {    
     return view('welcome');
 });
+
+
+//i.e http://localhost/firstlaravelapp/public/post/2
+Route::get('/post/{id}', function ($id) {    
+    return "this is post number ".$id;
+});
+
+
+//http://localhost/firstlaravelapp/public/post/2/ted
+Route::get('/post/{id}/{name}', function ($id,$name) {    
+    return "this is post number ".$id. ' name = '.$name ;
+});
+
